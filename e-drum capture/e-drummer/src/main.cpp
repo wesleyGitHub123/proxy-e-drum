6dcc011d401ac0633402f9b505dc780247b9a324
wesley <shomblerk123@gmail.com>
2026-07-06 22:35:13 +0800

    feat(capture): scaffold ESP32 firmware project

---
+++ e-drum capture/e-drummer/src/main.cpp
@@ -0,0 +1,10 @@
+#include <Arduino.h>
+
+void setup() {
+    Serial.begin(115200);
+    Serial.println("E-Drum Capture Box Initialized.");
+}
+
+void loop() {
+    // Real-time path will go here later
+}
